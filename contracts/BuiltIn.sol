b71d1ffa24f3d77971765ab05d0b2da9ad36cc7e
mtw-programmer <visnia58@gmail.com>
2023-12-07 16:34:52 +0100

    feat: add gas price and gas limit

--- contracts/BuiltIn.sol
+++ contracts/BuiltIn.sol
@@ -3,24 +3,33 @@
 pragma solidity ^0.8.19;
 
 contract BuiltIn {
+    /// @return Address interacting with contract
+    function getSender() public view returns (address) {
+        return msg.sender;
+    }
 
-  /// @return Address interacting with contract
-  function getSender() public view returns (address) {
-    return msg.sender;
-  }
+    /// @return Ether sent in the transaction
+    function getSentEther() public payable returns (uint) {
+        return msg.value;
+    }
 
-  /// @return Ether sent in the transaction
-  function getSentEther() public payable returns (uint) {
-    return msg.value;
-  }
+    /// @return Block difficulty
+    function getDifficulty() public view returns (uint) {
+        return block.prevrandao;
+    }
 
-  /// @return Block difficulty
-  function getDifficulty() public view returns (uint) {
-    return block.prevrandao;
-  }
+    /// @return Block timestamp
+    function getTimestamp() public view returns (uint) {
+        return block.timestamp;
+    }
 
-  /// @return Block timestamp
-  function getTimestamp() public view returns (uint) {
-    return block.timestamp;
-  }
+    /// @return Gas price
+    function getGas() public view returns (uint) {
+        return tx.gasprice;
+    }
+
+    /// @return Gas limit
+    function getGasFee() public view returns (uint) {
+        return block.gaslimit;
+    }
 }
